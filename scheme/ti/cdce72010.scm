f4d59952dfabc03c05a01ef4dd24cc390be23335
Frank Terbeck <ft@bewatermyfriend.org>
2011-07-13 01:00:46 +0200

    Add `toggle-trace' to (ti cdce72010) module
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/ti/cdce72010.scm
+++ scheme/ti/cdce72010.scm
@@ -1,5 +1,6 @@
 (define-module (ti cdce72010)
-  :export (read-registers))
+  :export (read-registers
+           toggle-trace))
 
 (use-modules (ti cdce-primitives))
 
@@ -10,3 +11,15 @@
       (if (< i 12)
           (loop (+ i 1))))
     a))
+
+(define (toggle-trace)
+  (display "-!- ")
+  (cond
+   (cdce/options:trace
+    (display "Dis")
+    (set! cdce/options:trace #f))
+   (else
+    (display "En")
+    (set! cdce/options:trace #t)))
+  (display "abling serial communication trace.")
+  (newline))
