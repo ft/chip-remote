2179bad59075875c895af8c6fef3d09de0bea709
Frank Terbeck <ft@bewatermyfriend.org>
2011-07-18 09:20:51 +0200

    Implement `output-index'
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/ti/cdce72010-validate.scm
+++ scheme/ti/cdce72010-validate.scm
@@ -24,7 +24,8 @@
 
 (define-module (ti cdce72010-validate)
   :export (divider?
-           mn-divider-value?))
+           mn-divider-value?
+           output-index?))
 
 (define (divider? idx)
   (and (> idx 0)
@@ -34,3 +35,7 @@
   (and (> value 0)
        ;; max. value is 14 bits set to `1' plus 1.
        (<= value 0b100000000000000)))
+
+(define (output-index? idx)
+  (and (>= idx 0)
+       (<= idx 9)))
