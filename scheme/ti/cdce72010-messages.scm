a945fe43a66557571073e24e392ed7b0f63eadac
Frank Terbeck <ft@bewatermyfriend.org>
2011-07-18 13:52:09 +0200

    Implement error-output-divider
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/ti/cdce72010-messages.scm
+++ scheme/ti/cdce72010-messages.scm
@@ -24,7 +24,8 @@
 
 (define-module (ti cdce72010-messages)
   :export (error-divider
-           error-mn-divider))
+           error-mn-divider
+           error-output-index))
 
 (define (error-divider x)
   (display "`divider' needs to be an integer 1..8.\n")
@@ -34,3 +35,6 @@
   (display (format #f "The M and N dividers need to be set from 1 to ~d.\n"
                    (#b100000000000000)))
   (display (format #f "You tried to set: ~d\n" value)))
+
+(define (error-output-index)
+  (display "Output index out of range (0..9)\n"))
