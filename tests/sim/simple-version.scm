760aef88b7a885f4a03acc4e0869f4d045238cf0
Frank Terbeck <ft@bewatermyfriend.org>
2012-08-02 21:50:02 +0200

    Add a new kind of tests
    Namely tests that run through the board simulations --fork option.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ tests/sim/simple-version.scm
@@ -0,0 +1,14 @@
+;; The stuff we want to test is implemented in the following module.
+(use-modules (chip-remote protocol))
+
+;; The `$CR_BOARD_DEVICE' environment variable is set to the device, the
+;; board-simulation program set up for us to connect to.
+(define device (getenv "CR_BOARD_DEVICE"))
+
+;; Open it, say hello, query the protocol version, say bye, close up and exit.
+(cr/open device)
+(hi)
+(version)
+(bye)
+(cr/close)
+(quit 0)
