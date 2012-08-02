980ab07791ad4d140f17541e26e477f3c1d9061b
Frank Terbeck <ft@bewatermyfriend.org>
2012-08-02 23:03:15 +0200

    Rename upon import
    (chip-remote protocol) overrides Guile's core `version' function, to
    avoid that, import the module with a symbol renamer.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- tests/sim/simple-version.scm
+++ tests/sim/simple-version.scm
@@ -1,5 +1,6 @@
 ;; The stuff we want to test is implemented in the following module.
-(use-modules (chip-remote protocol))
+(use-modules ((chip-remote protocol)
+              #:renamer (symbol-prefix-proc 'chip-remote/)))
 
 ;; The `$CR_BOARD_DEVICE' environment variable is set to the device, the
 ;; board-simulation program set up for us to connect to.
@@ -8,14 +9,14 @@
 ;; Open it, say hello, query the protocol version, say bye, close up and exit.
 (or (cr/open device)
     (throw 'open-failed))
-(or (hi)
+(or (chip-remote/hi)
     (throw 'hi-failed))
 
-(let ((v (version)))
+(let ((v (chip-remote/version)))
   (or v
       (throw 'version-failed `(version ,v))))
 
-(or (bye)
+(or (chip-remote/bye)
     (throw 'bye-failed))
 (or (cr/close)
     (throw 'close-failed))
