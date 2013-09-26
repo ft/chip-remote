86560dc34c8b51c76a3f03512bd2985a32c8fe7e
Frank Terbeck <ft@bewatermyfriend.org>
2013-09-26 14:50:41 +0200

    fw: Introduce xmalloc macro
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- remote-fw/utils.h
+++ remote-fw/utils.h
@@ -7,6 +7,7 @@
 #define STREQ(x, y) (!strcmp(x, y))
 #define STREQ_N(x, y, n) (!strncmp(x, y, n))
 #define _(x) ((const char *)x)
+#define xmalloc(n, t) ((t *)malloc(n * sizeof(t)))
 
 uint32_t str2uint(const char *, int, int *);
 void uint2str(uint32_t, char *);
