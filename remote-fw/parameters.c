61627fe37ea8b689c35e057cbb3fcba248294345
Frank Terbeck <ft@bewatermyfriend.org>
2013-09-26 14:54:32 +0200

    fw: Add parameters code
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ remote-fw/parameters.c
@@ -0,0 +1,17 @@
+#include "chip-remote.h"
+#include "parameters.h"
+
+void
+cr_destroy_params(struct cr_parameter *params)
+{
+}
+
+void
+cr_port_reset_line_roles(struct cr_port *port)
+{
+}
+
+void
+cr_destroy_port_map(struct cr_port *port)
+{
+}
---
+++ remote-fw/parameters.h
@@ -0,0 +1,8 @@
+#ifndef INC_PARAMETERS_H
+#define INC_PARAMETERS_H
+
+void cr_destroy_params(struct cr_parameter *);
+void cr_port_reset_line_roles(struct cr_port *);
+void cr_destroy_port_map(struct cr_port *);
+
+#endif /* INC_PARAMETERS_H */
