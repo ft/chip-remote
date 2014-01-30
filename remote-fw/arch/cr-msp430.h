10cedf28b84e5afc3cc6ccd2267cb0bf320b5c57
Frank Terbeck <ft@bewatermyfriend.org>
2014-01-30 16:24:03 +0100

    fw: Prepare cross-compiler awareness for msp430 builds
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- remote-fw/arch/cr-msp430.h
+++ remote-fw/arch/cr-msp430.h
@@ -5,4 +5,37 @@
 
 int access_port1(struct cr_line *, enum cr_access_mode, int);
 
+#ifdef MSPGCC_BUILD
+
+/*
+ * This defines XINTERRUPT and INTERRUPT_PROTOTYPE for use with mspgcc.
+ */
+
+#define XINTERRUPT(VECTOR, SYMBOL)                              \
+    /* prototype to shut up the compiler */                     \
+    void __attribute__((__interrupt__(VECTOR))) SYMBOL(void);   \
+    void __attribute__((__interrupt__(VECTOR))) SYMBOL(void)
+
+#define INTERRUPT_PROTOTYPE(VECTOR, SYMBOL)                     \
+    void __attribute__((__interrupt__(VECTOR)))  SYMBOL(void);
+
+#else /* MSPGCC_BUILD */
+
+/*
+ * This defines XINTERRUPT and INTERRUPT_PROTOTYPE for use with Texas
+ * Intrument's C compiler from its Code Composer Studio platform.
+ *
+ * This also needs this before:
+ *
+ *         #pragma vector=VECTOR
+ */
+
+#define XINTERRUPT(VECTOR, SYMBOL) \
+    __interrupt void SYMBOL(void)
+
+#define INTERRUPT_PROTOTYPE(VECTOR, SYMBOL)     \
+    __interrupt void SYMBOL(void);
+
+#endif /* !MSPGCC_BUILD */
+
 #endif /* INC_CR_MSP430F1481_H */
