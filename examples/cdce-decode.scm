78c47973638cb0b51dc81d1813eec515102df5a7
Frank Terbeck <ft@bewatermyfriend.org>
2014-02-13 23:32:28 +0100

    Rewrite ‘examples/cdce-decode.scm’ a bit
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- examples/cdce-decode.scm
+++ examples/cdce-decode.scm
@@ -2,29 +2,34 @@
 ;;
 ;; Terms for redistribution and use can be found in LICENCE.
 
-(use-modules
- ((chip-remote devices ti cdce72010)
-  #:renamer (symbol-prefix-proc 'cdce/))
- ((chip-remote devices ti cdce72010 decode)
-  #:renamer (symbol-prefix-proc 'cdce/)))
+(use-modules ((chip-remote devices ti cdce72010)
+              #:renamer (symbol-prefix-proc 'cdce/))
+             ((chip-remote devices ti cdce72010 decode)
+              #:renamer (symbol-prefix-proc 'cdce/)))
 
-(define foo '(#x683c0ed0
-              #x68000051
-              #x68000002
-              #xeb800003
-              #xe90c0004
-              #x68000005
-              #x68000006
-              #x68000117
-              #x69800158
-              #xeb000049
-              #x009c027a
-              #x8800848b
-              #x66009f0c))
+;; These are the factory-default values for CDCE72010 devices. You can
+;; obviously takea look at the reference documentation about what these
+;; register values represent...
+(define factory-defaults '(#x683c0ed0
+                           #x68000051
+                           #x68000002
+                           #xeb800003
+                           #xe90c0004
+                           #x68000005
+                           #x68000006
+                           #x68000117
+                           #x69800158
+                           #xeb000049
+                           #x009c027a
+                           #x8800848b
+                           #x66009f0c))
 
-(let next ((reg foo))
-  (cond ((null? reg)
+;; ...or, you could just iterate across the values from the previously defined
+;; list and use the decoding module and let it tell you what the register
+;; values mean.
+(let next ((regs factory-defaults))
+  (cond ((null? regs)
          (quit 0))
         (else
-         (cdce/decode-register-by-value (car reg))
-         (next (cdr reg)))))
+         (cdce/decode-register-by-value (car regs))
+         (next (cdr regs)))))
