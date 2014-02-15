0fcd297143195c96f8bea13186fa3c1285bdc4bb
Frank Terbeck <ft@bewatermyfriend.org>
2014-02-15 17:05:57 +0100

    More examples, including instructions
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ examples/init-port.scm
@@ -0,0 +1,39 @@
+;; Copyright (c) 2014 chip-remote workers, All rights reserved.
+;;
+;; Terms for redistribution and use can be found in LICENCE.
+
+(use-modules (chip-remote io)
+             (chip-remote protocol))
+
+(define connection (make-cr-connection serial-device))
+
+(define port-index 0)
+
+(define pin-chip-select 0)
+(define pin-clock 1)
+(define pin-master-out-slave-in 2)
+(define pin-master-in-slave-out 3)
+
+(io-open connection)
+(hi connection)
+
+(map (lambda (x) (set connection port-index (car x) (cdr x)))
+     '((mode . spi)
+       (frame-length . 32)
+       (bit-order . lsb-first)
+       (clk-phase-delay . 1)
+       (clk-polarity . idle-low)
+       (cs-polarity . active-low)
+       (cs-lines . 1)))
+
+(map (lambda (x) (line connection port-index (cdr x) (car x)))
+     `((cs . ,pin-chip-select)
+       (clk . ,pin-clock)
+       (mosi . ,pin-master-out-slave-in)
+       (miso . ,pin-master-in-slave-out)))
+
+(init connection port-index)
+(focus connection port-index)
+
+(bye connection)
+(io-close connection)
---
+++ examples/init-serial.scm
@@ -0,0 +1,23 @@
+;; Copyright (c) 2014 chip-remote workers, All rights reserved.
+;;
+;; Terms for redistribution and use can be found in LICENCE.
+
+;; In the full example in ‘cdce-read-all-registers.scm’ the ‘stty’ is used to
+;; setup parameters of the serial link to a remote board, since Guile doesn't
+;; have support for termios built-in.
+;;
+;; There is however <https://github.com/ft/guile-termios> which implements such
+;; an interface. And this script uses that interface to setup these parameters.
+
+(use-modules (termios)
+             (termios system))
+
+(define tty (open-io-file serial-device))
+(define ts (make-termios-struct))
+
+;; This is 8N1 with 921600bd symbol-rate.
+(cf-make-raw! ts)
+(cf-set-speed! ts termios-B921600)
+(tc-set-attr tty ts)
+
+(close-port tty)
