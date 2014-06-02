ce3cde06e9039ae93812b6127a9f2a1d6c67a0d7
Frank Terbeck <ft@bewatermyfriend.org>
2014-06-02 22:39:54 +0200

    scheme: Add functions for discovering generated functions
    By entry name, that is. Usually, you'd just use the function or
    variable directly. But in case you need to do a NAME => FUNC/VAR
    lookup, these functions can do the trick.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/chip-remote.scm
+++ scheme/chip-remote.scm
@@ -3,7 +3,12 @@
 ;; Terms for redistribution and use can be found in LICENCE.
 
 (define-module (chip-remote)
-  #:export (chain-fncs))
+  #:use-module (ice-9 optargs)
+  #:export (chain-fncs
+
+            bit-getter-of
+            bit-setter-of
+            register-address-of))
 
 ;; Functions that alter packs of bits in register values look like this:
 ;;
@@ -39,3 +44,69 @@
                (syntax-case fnc ()
                  ((f args ...) #'(f rest args ...))
                  (f #'(f rest))))))))))
+
+(define (variable-of name module generator renamer exception)
+  (let* ((vname (renamer (generator name)))
+         (var (module-variable module vname)))
+    (if (not var)
+        (throw exception name vname)
+        (variable-ref var))))
+
+(define* (register-address-of name #:key
+                              (renamer (lambda (x) x))
+                              (module (current-module)))
+  "Discover register address for the NAME field in the register map defined by
+`define-register-map'. Consider this:
+
+  (define-register-map foo
+    (0 (contents (gain 0 4))))
+
+Usually, you would use `regaddr:gain' to get \"0\" - the address for `gain'.
+But in cases where you need to computationally tie the name `gain' to the
+address in `regaddr:again', this function can be used. There are three basic
+situations:
+
+  1. You imported the module containing the corresponding `define-register-map'
+     regularly:
+
+        (use-modules (chip-remote devices xxx foo registers))
+
+        (register-address-of 'gain) => 0
+
+  2. You imported the module with a symbol renamer in place:
+
+        (use-modules ((chip-remote devices xxx foo registers)
+                      #:renamer (symbol-prefix-proc 'bar/)))
+
+        (register-address-of 'gain #:renamer (symbol-prefix-proc 'bar/)
+          => 0
+
+  3. You want the address of `gain' from a module, you didn't import:
+
+        (register-address-of 'gain
+          #:module (chip-remote devices xxx foo registers)
+          => 0"
+  (variable-of name module
+               (lambda (x) (symbol-append 'regaddr: x))
+               renamer
+               'cr-no-such-register-address))
+
+(define* (bit-getter-of name #:key
+                        (renamer (lambda (x) x))
+                        (module (current-module)))
+  "Like `register-address-of', but instead returns the getter function for a
+register map entry."
+  (variable-of name module
+               (lambda (x) (symbol-append 'get- x '-bits))
+               renamer
+               'cr-no-such-getter-function))
+
+(define* (bit-setter-of name #:key
+                        (renamer (lambda (x) x))
+                        (module (current-module)))
+  "Like `register-address-of', but instead returns the setter function for a
+register map entry."
+  (variable-of name module
+               (lambda (x) (symbol-append 'set- x '-bits))
+               renamer
+               'cr-no-such-setter-function))
