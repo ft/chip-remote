3405ee2bab5d4564857118221cf60046beefeffe
Frank Terbeck <ft@bewatermyfriend.org>
2014-06-05 13:22:24 +0200

    examples: Rewrite ‘ads4149-decode-defaults’ using ‘device-decoder’
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- examples/ads4149-decode-defaults.scm
+++ examples/ads4149-decode-defaults.scm
@@ -10,17 +10,33 @@
 ;; the generated data to stdout.
 
 (use-modules (chip-remote devices ti ads4149 registers)
-             (chip-remote decode to-text)
+             (chip-remote decode)
+             (chip-remote level-3)
              (chip-remote register-map))
 
-(format #t "Default register-map of TI's ADS4149 ADU:~%")
+(format #t "Default register-map of TI's ADS4149 ADU:~%~%")
 
-(map (lambda (x)
-       (newline)
-       (display-list
-        (register->text #:register-map ads4149-register-map
-                        #:address x
-                        #:width register-width
-                        #:value (register-default ads4149-register-map x)
-                        #:colour? #t)))
-     (map car ads4149-register-map))
+;; Shorthands:
+(define rm ads4149-register-map)
+(define rw register-width)
+(define ic ads4149-regmap-interconnections)
+
+(device-decoder #:register-map rm
+                #:width rw
+                #:interconnections ic
+                ;; Let's force colour output in this case:
+                #:colour? #t
+                ;; Decoders are easy: They need a register-map, an address and
+                ;; a value, and then they'll do their thing. The results are
+                ;; best when the register-map is properly annotated. The
+                ;; register-map of the ADS4149 is.
+                #:decoder (lambda (addr value) (decode rm addr value))
+                ;; The reader function takes one argument: An address. It takes
+                ;; that and produces a corresponding register value. It can do
+                ;; that by querying a live device on a board. But it doesn't
+                ;; have to. Since we want the default values of the registers
+                ;; (and the register map of the ADS4149 defines one for every
+                ;; register), we can also just look up the default value in the
+                ;; register-map and return that, thus fulfilling the purpose of
+                ;; this script.
+                #:reader (lambda (addr) (regmap->item rm addr 'default-value)))
