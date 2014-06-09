d53cae2b161270e90771ae1cb22693adbf77b474
Frank Terbeck <ft@bewatermyfriend.org>
2014-06-09 23:23:41 +0200

    scheme: Add a module that handles keyword-association lists
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ scheme/chip-remote/keyword-assoc.scm
@@ -0,0 +1,12 @@
+;; Copyright (c) 2014 chip-remote workers, All rights reserved.
+;;
+;; Terms for redistribution and use can be found in LICENCE.
+
+(define-module (chip-remote keyword-assoc)
+  #:export (kwa-ref))
+
+(define (kwa-ref kw-lst kw)
+  (let ((m (memq kw kw-lst)))
+    (if (not m)
+        (throw 'cr-missing-keyword-entry kw-lst kw)
+        (cadr m))))
