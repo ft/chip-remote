57b06423c8cf0f2251939cd5d47ab195a8f3313e
Frank Terbeck <ft@bewatermyfriend.org>
2014-08-09 11:54:28 +0200

    scheme: Add more default bit-decoders
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/chip-remote/bit-decoders.scm
+++ scheme/chip-remote/bit-decoders.scm
@@ -4,12 +4,24 @@
 
 (define-module (chip-remote bit-decoders)
   #:use-module (ice-9 format)
-  #:export (literal-binary
+  #:export (binwidth->hexwidth
+            boolean-status
+            boolean-status-inverted
+            literal-binary
+            literal-hex
             logic-active-high
             logic-active-low
+            power-of-two
             reverse-lookup
             twos-complement
             unsigned-integer))
+
+(define (binwidth->hexwidth n)
+  (let ((mod (modulo n 4)))
+    (+ (floor (/ n 4)) (if (> mod 0) 1 0))))
+
+(define (power-of-two n o w value)
+  (expt 2 value))
 
 (define (unsigned-integer n o w value)
   value)
@@ -24,11 +36,20 @@
 (define (literal-binary name offset width value)
   (format #f "~v,'0b" width value))
 
+(define (literal-hex name offset width value)
+  (format #f "0x~v,'0x" (binwidth->hexwidth width) value))
+
 (define (logic-active-high n o w x)
   (if (= 0 x) #f #t))
 
 (define (logic-active-low n o w x)
   (if (= 0 x) #t #f))
+
+(define (boolean-status n o w x)
+  (if (zero? x) 'false 'true))
+
+(define (boolean-status-inverted n o w x)
+  (if (zero? x) 'true 'false))
 
 (define (reverse-lookup valmap value)
   (let next ((cur valmap))
