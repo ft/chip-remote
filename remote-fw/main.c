02196b9e553c3194e4b7edf307715413cf6cff74
Frank Terbeck <ft@bewatermyfriend.org>
2014-09-12 18:58:49 +0200

    fw: Define cr-ports for the adu1e9 project
    See comments in code for details.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- remote-fw/main.c
+++ remote-fw/main.c
@@ -49,7 +49,74 @@
 
 #include "arch/cr-msp430.h"
 
+/*
+ * Ports in the adu1e9 project:
+ *
+ * These are the links to the FPGA:
+ *
+ * P1.0 -> NC    P2.0 -> L22   P3.0 -> DIP0
+ * P1.1 -> NC    P2.1 -> M21   P3.1 -> DIP1
+ * P1.2 -> NC    P2.2 -> P20   P3.2 -> DIP2
+ * P1.3 -> NC    P2.3 -> P19   P3.3 -> DIP3
+ * P1.4 -> K23   P2.4 -> P26   P3.4 -> UART_TX
+ * P1.5 -> L24   P2.5 -> R26   P3.5 -> UART_RX
+ * P1.6 -> L23   P2.6 -> N24   P3.6 -> LED0
+ * P1.7 -> M24   P2.7 -> P25   P3.7 -> LED1
+ *
+ * P4.0 -> C24   P5.0 -> R22   P6.0 -> T25
+ * P4.1 -> B25   P5.1 -> T24   P6.1 -> N26
+ * P4.2 -> M19   P5.2 -> T23   P6.2 -> M25
+ * P4.3 -> M22   P5.3 -> T22   P6.3 -> L25
+ * P4.4 -> N23   P5.4 -> R21   P6.4 -> K26
+ * P4.5 -> P24   P5.5 -> R20   P6.5 -> L26
+ * P4.6 -> P23   P5.6 -> B26   P6.6 -> K25
+ * P4.7 -> R23   P5.7 -> D25   P6.7 -> M26
+ *
+ * That's 5*8 - 4 = 36 pins.
+ *
+ * There are three devices using a regular SPI interface. One device with SPI
+ * plus five other pins. The FPGA is controlled via the parex interface, that
+ * needs CLK, 3*ADDR and 8*DATA pins. Thus:
+ *
+ * 4*4 + 5 + 1 + 3 + 8 pins = 33 pins.
+ *
+ * So lets define three 4-pin ports; one 9 pin port (since we got 3 pins left,
+ * let's make this one 12 pins wide anyway) and one 12 pin port.
+ */
+
 static struct cr_line port1_lines[] = {
+    NEW_LINE(access_port1, dir_port1, 1<<4),
+    NEW_LINE(access_port1, dir_port1, 1<<5),
+    NEW_LINE(access_port1, dir_port1, 1<<6),
+    NEW_LINE(access_port1, dir_port1, 1<<7),
+    LINE_LIST_END
+};
+
+static struct cr_line port2_lines[] = {
+    NEW_LINE(access_port2, dir_port2, 1<<0),
+    NEW_LINE(access_port2, dir_port2, 1<<1),
+    NEW_LINE(access_port2, dir_port2, 1<<2),
+    NEW_LINE(access_port2, dir_port2, 1<<3),
+    LINE_LIST_END
+};
+
+static struct cr_line port3_lines[] = {
+    NEW_LINE(access_port2, dir_port2, 1<<4),
+    NEW_LINE(access_port2, dir_port2, 1<<5),
+    NEW_LINE(access_port2, dir_port2, 1<<6),
+    NEW_LINE(access_port2, dir_port2, 1<<7),
+    LINE_LIST_END
+};
+
+static struct cr_line port4_lines[] = {
+    NEW_LINE(access_port4, dir_port4, 1<<0),
+    NEW_LINE(access_port4, dir_port4, 1<<1),
+    NEW_LINE(access_port4, dir_port4, 1<<2),
+    NEW_LINE(access_port4, dir_port4, 1<<3),
+    NEW_LINE(access_port4, dir_port4, 1<<4),
+    NEW_LINE(access_port4, dir_port4, 1<<5),
+    NEW_LINE(access_port4, dir_port4, 1<<6),
+    NEW_LINE(access_port4, dir_port4, 1<<7),
     NEW_LINE(access_port5, dir_port5, 1<<0),
     NEW_LINE(access_port5, dir_port5, 1<<1),
     NEW_LINE(access_port5, dir_port5, 1<<2),
@@ -57,8 +124,29 @@ static struct cr_line port1_lines[] = {
     LINE_LIST_END
 };
 
+static struct cr_line port5_lines[] = {
+    NEW_LINE(access_port5, dir_port5, 1<<4),
+    NEW_LINE(access_port5, dir_port5, 1<<5),
+    NEW_LINE(access_port5, dir_port5, 1<<6),
+    NEW_LINE(access_port5, dir_port5, 1<<7),
+    NEW_LINE(access_port6, dir_port6, 1<<0),
+    NEW_LINE(access_port6, dir_port6, 1<<1),
+    NEW_LINE(access_port6, dir_port6, 1<<2),
+    NEW_LINE(access_port6, dir_port6, 1<<3),
+    NEW_LINE(access_port6, dir_port6, 1<<4),
+    NEW_LINE(access_port6, dir_port6, 1<<5),
+    NEW_LINE(access_port6, dir_port6, 1<<6),
+    NEW_LINE(access_port6, dir_port6, 1<<7),
+    LINE_LIST_END
+};
+
 struct cr_port cr_ports[] = {
-    NEW_PORT(4, port1_lines)
+    NEW_PORT(4, port1_lines),
+    NEW_PORT(4, port2_lines),
+    NEW_PORT(4, port3_lines),
+    NEW_PORT(12, port4_lines),
+    NEW_PORT(12, port5_lines),
+    PORT_LIST_END
 };
 
 #endif /* CR_MSP430F1481 */
