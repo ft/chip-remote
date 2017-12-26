d0a84a0af5468195f43ca14c8fc4a0ebdcb26729
Frank Terbeck <ft@bewatermyfriend.org>
2017-12-26 13:44:03 +0100

    Add a minimal interpreted language
    This may seem heavy-handed, but it has its purpose: Since chip-remote's goal is
    not only to play with device configurations in Scheme, but also to be able to
    compile down to other target languages such as C or C++, it is important to
    keep information about source code, in case custom solutions such as encoders,
    decoders or validators need to be implemented.
    
    This could be done by quoting Scheme expressions and evaling those, but Scheme
    is quite powerful and its full specs are hard to compile to C or something
    similar. This interpreter has quite a limited feature set and its evaluator
    yields Scheme values, so its output can be used within the host language.
    
    A new type <evaluation> is introduced that keeps the source-code for the
    interpreter alongside the value yielded by the evaluator. We'll store this, for
    instance, in custom #:semantics blocks so compiling to other languages is
    possible.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ scheme/chip-remote/interpreter.scm
@@ -0,0 +1,80 @@
+;; Copyright (c) 2017 chip-remote workers, All rights reserved.
+;;
+;; Terms for redistribution and use can be found in LICENCE.
+
+(define-module (chip-remote interpreter)
+  #:use-module (srfi srfi-9)
+  #:use-module (ice-9 match)
+  #:export (cr-eval
+            make-evaluation
+            define-evaluation))
+
+;; Sometimes, an item in a register has a simple arithmetic relation to its
+;; semantic value. This interpreter can be used to express these relations.
+;;
+;; The reason, we did not opt to just use a scheme procedure to express these
+;; relations, is that it is much harder to compile all of scheme to other
+;; languages, like C or C++. This very minimal language is easy to compile to
+;; something else, and it is almost trivial to interpret in our executing
+;; Scheme environment.
+
+(define (default-environment x)
+  (error (format #f "Unknown variable: ~s" x)))
+
+(define (cr-eval expression)
+  (cr-eval* expression default-environment))
+
+(define (cr-eval* expression environment)
+  (let ((just-eval (lambda (e) (cr-eval* e environment))))
+    (match expression
+      ((? number? n) n)
+      (('increment e1) (+ 1 (just-eval e1)))
+      (('increment e1 e2) (+ (just-eval e1) (just-eval e2)))
+      (('decrement e1) (- (just-eval e1) 1))
+      (('decrement e1 e2) (- (just-eval e1) (just-eval e2)))
+      (('multiply e1 e2) (* (just-eval e1) (just-eval e2)))
+      (('integer-divide e1 e2) (truncate (/ (just-eval e1)
+                                            (just-eval e2))))
+      (('left-shift target how-much)
+       (ash (just-eval target) (just-eval how-much)))
+      (('right-shift target how-much)
+       (ash (just-eval target) (* -1 (just-eval how-much))))
+      ((e1 'greater-than? e2) (> (just-eval e1) (just-eval e2)))
+      ((e1 'less-than? e2) (< (just-eval e1) (just-eval e2)))
+      ((e1 'equal-to? e2) (= (just-eval e1) (just-eval e2)))
+      (('if condition consequence alternative)
+       (if (just-eval condition)
+           (just-eval consequence)
+           (just-eval alternative)))
+      ((? symbol? b) (environment b))
+      ;; This interpreter supports functions with one or two arguments. This
+      ;; can likely be implemented more elegantly. But I won't bother for now.
+      (('lambda ((? symbol? x)) body)
+       (lambda (arg)
+         (cr-eval* body (lambda (y)
+                          (if (eq? x y)
+                              arg
+                              (environment y))))))
+      (('lambda ((? symbol? a) (? symbol? b)) body)
+       (lambda (arg1 arg2)
+         (cr-eval* body (lambda (x)
+                          (cond ((eq? x a) arg1)
+                                ((eq? x b) arg2)
+                                (else (environment x)))))))
+      ((rator rand) ((just-eval rator) (just-eval rand)))
+      ((rator rand1 rand2) ((just-eval rator)
+                            (just-eval rand1)
+                            (just-eval rand2)))
+      (_ (error (format #f "Invalid expression: ~s~%" expression))))))
+
+(define-record-type <evaluation>
+  (make-evaluation* expression value)
+  evaluation?
+  (expression evaluation-expression)
+  (value evaluation-value))
+
+(define (make-evaluation e)
+  (make-evaluation* e (cr-eval e)))
+
+(define-syntax-rule (define-evaluation binding e)
+  (define binding (make-evaluation e)))
