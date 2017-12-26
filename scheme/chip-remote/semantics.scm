024e45215d15334fb678be0cf48794edf09d4951
Frank Terbeck <ft@bewatermyfriend.org>
2017-12-26 13:51:29 +0100

    Add a semantics type
    This can be used to describe the semantics of a register's item. A number of
    common types, as well as entry points for the interpreter's language and also
    raw Scheme code are provided.
    
    Note that the table-lookup based codecs are not implemented yet.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ scheme/chip-remote/semantics.scm
@@ -0,0 +1,51 @@
+;; Copyright (c) 2017 chip-remote workers, All rights reserved.
+;;
+;; Terms for redistribution and use can be found in LICENCE.
+
+(define-module (chip-remote semantics)
+  #:use-module (ice-9 optargs)
+  #:use-module (srfi srfi-9)
+  #:use-module (chip-remote codecs)
+  #:use-module (chip-remote interpreter)
+  #:export (make-semantics
+            semantics?
+            semantics-type
+            semantics-decode
+            semantics-encode))
+
+(define-record-type <semantics>
+  (make-semantics* type decode encode)
+  semantics?
+  (type semantics-type)
+  (decode semantics-decode)
+  (encode semantics-encode))
+
+(define* (make-semantics type #:key (decode identity) (encode identity))
+  (case type
+    ((boolean) (make-semantics* type decode-boolean encode-boolean))
+    ((boolean/active-low) (make-semantics* type
+                                           decode-boolean/active-low
+                                           encode-boolean/active-low))
+    ((state) (make-semantics* type decode-state encode-state))
+    ((state/active-low) (make-semantics* type
+                                         decode-state/active-low
+                                         encode-state/active-low))
+    ((unsigned-integer) (make-semantics* type identity identity))
+    ((offset-binary) (make-semantics* type
+                                      decode-offset-binary
+                                      encode-offset-binary))
+    ((ones-complement) (make-semantics* type
+                                        decode-ones-complement
+                                        encode-ones-complement))
+    ((twos-complement) (make-semantics* type
+                                        decode-twos-complement
+                                        encode-twos-complement))
+    ((sign-magnitude) (make-semantics* type
+                                       decode-sign-magnitude
+                                       encode-sign-magnitude))
+    ((table-lookup) (make-semantics* type decode-with-table encode-with-table))
+    ((interpreter) (make-semantics* type
+                                    (make-evaluation decode)
+                                    (make-evaluation encode)))
+    ((scheme) (make-semantics* type decode encode))
+    (else (throw 'unknown-semantics type decode encode))))
