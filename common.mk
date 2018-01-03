73fff9781a3b15abf380e12403fa2477d82a66ee
Frank Terbeck <ft@bewatermyfriend.org>
2018-01-03 13:23:24 +0100

    build system: Reimplement byte compilation
    This only builds the new code and ignores the old. At some point all the code
    base will migrate to work with the new data representations and be built once
    again.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ common.mk
@@ -0,0 +1,30 @@
+LOAD_PATH = $(TOPDIR)/scheme
+TEST_PATH = $(TOPDIR)/tests
+GUILE_BINARY = guile
+GUILE_CALL = $(GUILE_BINARY) -L $(LOAD_PATH) -C $(LOAD_PATH) --no-auto-compile
+GUILD_BINARY = guild
+
+CR = $(LOAD_PATH)/chip-remote
+
+MODULES_CORE = $(CR)/bit-operations.scm
+MODULES_CORE += $(CR)/codecs.scm
+MODULES_CORE += $(CR)/decode.scm
+MODULES_CORE += $(CR)/device.scm
+MODULES_CORE += $(CR)/interpreter.scm
+MODULES_CORE += $(CR)/manufacturer.scm
+MODULES_CORE += $(CR)/page-map.scm
+MODULES_CORE += $(CR)/process-plist.scm
+MODULES_CORE += $(CR)/register-map.scm
+MODULES_CORE += $(CR)/register.scm
+MODULES_CORE += $(CR)/semantics.scm
+MODULES_CORE += $(CR)/utilities.scm
+
+MODULES_MANUFACTURERS = $(CR)/manufacturer/bosch.scm
+MODULES_MANUFACTURERS += $(CR)/manufacturer/linear-technology.scm
+MODULES_MANUFACTURERS += $(CR)/manufacturer/texas-instruments.scm
+
+MODULES_DEVICES = $(CR)/devices/bosch/bno055.scm
+MODULES_DEVICES += $(CR)/devices/linear-technology/ltc6603.scm
+MODULES_DEVICES += $(CR)/devices/texas-instruments/cdce72010.scm
+
+MODULES= $(MODULES_CORE) $(MODULES_MANUFACTURERS) $(MODULES_DEVICES)
