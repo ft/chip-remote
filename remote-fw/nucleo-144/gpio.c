526957eee9328836fddeb2e19f85d2ed491a2d9f
Frank Terbeck <ft@bewatermyfriend.org>
2018-10-08 22:37:39 +0200

    fw: To use GPIOE its clock domain must be active
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- remote-fw/nucleo-144/gpio.c
+++ remote-fw/nucleo-144/gpio.c
@@ -13,6 +13,7 @@ board_gpio_init(void)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
     __HAL_RCC_GPIOG_CLK_ENABLE();
 
     HAL_GPIO_WritePin(GPIOB, LD3_Pin | LD2_Pin, GPIO_PIN_RESET);
