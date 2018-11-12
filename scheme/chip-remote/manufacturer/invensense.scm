0b34936d751837b351b2e2793633cac0a8e2089e
Frank Terbeck <ft@bewatermyfriend.org>
2018-11-12 20:17:47 +0100

    Add device description for InvenSense's ICM-20602 motion unit
    There's not much in terms of semantics yet, but a full transcription of the
    chip's register table.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ scheme/chip-remote/manufacturer/invensense.scm
@@ -0,0 +1,12 @@
+;; Copyright (c) 2018 chip-remote workers, All rights reserved.
+;;
+;; Terms for redistribution and use can be found in LICENCE.
+
+(define-module (chip-remote manufacturer invensense)
+  #:use-module (chip-remote manufacturer)
+  #:export (invensense))
+
+(define-manufacturer invensense
+  #:name "InvenSense"
+  #:homepage "http://invensense.com"
+  #:wikipedia "https://en.wikipedia.org/wiki/InvenSense")
