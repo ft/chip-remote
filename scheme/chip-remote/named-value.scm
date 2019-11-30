87cc1d768a6ac11c22fba2e39636032dec5d48ed
Frank Terbeck <ft@bewatermyfriend.org>
2019-11-30 17:00:42 +0100

    Add pretty-printer for named-value
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/chip-remote/named-value.scm
+++ scheme/chip-remote/named-value.scm
@@ -4,6 +4,7 @@
 
 (define-module (chip-remote named-value)
   #:use-module (srfi srfi-9 gnu)
+  #:use-module (chip-remote pretty-print)
   #:export (make-named-value
             define-value
             identify-value
@@ -16,6 +17,18 @@
   named-value?
   (name value-name identify-value)
   (value value-data))
+
+(define (pp-named-value port indent named-value)
+  (let ((pp (make-printer port indent))
+        (cplx (make-printer/object port indent)))
+    (pp-record port 'named-value
+               (lambda ()
+                 (pp 'name (value-name named-value))
+                 (cplx 'value (value-data named-value))))))
+
+(set-record-type-printer! <named-value>
+  (lambda (rec port)
+    (pp-named-value port (pp-indent) rec)))
 
 (define-syntax-rule (define-value binding expr)
   (define binding (make-named-value 'binding expr)))
