a611539367ca64264d8d82b25349631910972c59
Frank Terbeck <ft@bewatermyfriend.org>
2021-02-28 23:39:21 +0100

    fw: Use utility function module in transmit command
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- firmware/libraries/libchip-remote/command-transmit.c
+++ firmware/libraries/libchip-remote/command-transmit.c
@@ -9,58 +9,17 @@
  * @brief Implemenation of TRANSMIT command
  */
 
-#include <stdio.h>
-
 #include <common/compiler.h>
 
 #include <commands.h>
 #include <commands-private.h>
 #include <cr-port.h>
+#include <cr-utilities.h>
 
 struct cr_argument transmit_arguments[] = {
     { .optional = false, .type = CR_PROTO_ARG_TYPE_INTEGER },
     { .optional = false, .type = CR_PROTO_ARG_TYPE_VOID }
 };
-
-static void
-uint2str(uint32_t num, char *buf)
-{
-    static char chtable[] = {
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        'a', 'b', 'c', 'd', 'e', 'f' };
-
-    /* buf needs to be able to hold 8+1 characters. */
-    int i, max, step;
-
-    for (i = 7; i >= 0; --i) {
-        step = 4 * i;
-        if (num & (uint32_t)(0xful << step)) {
-            max = i;
-            break;
-        }
-    }
-
-    if (i < 0) {
-        buf[0] = '0';
-        buf[1] = '\0';
-        return;
-    }
-
-    buf[i+1] = '\0';
-    for (i = max; i >= 0; --i) {
-        step = 4 * i;
-        buf[max-i] = chtable[(num & (uint32_t)(0xful << step)) >> step];
-    }
-    buf[8] = '\0';
-}
-
-static void
-put_u32(const struct cr_protocol *proto, const uint32_t value)
-{
-    char buffer[9u];
-    uint2str(value, buffer);
-    proto->reply(buffer);
-}
 
 static inline struct cr_port*
 current_port(const struct cr_protocol *proto)
@@ -76,7 +35,7 @@ cr_handle_transmit(const struct cr_protocol *proto,
 {
     uint32_t rx;
     cr_transmit(current_port(proto), arg[0].data.u32, &rx);
-    put_u32(proto, rx);
-    proto->reply("\n");
+    cr_proto_put_u32(proto, rx);
+    cr_proto_put_newline(proto);
     return CR_PROTO_STATE_ACTIVE;
 }
