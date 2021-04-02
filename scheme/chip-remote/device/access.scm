d4f8f831b424bbf02fc4cd49bbc6f272ac607220
Frank Terbeck <ft@bewatermyfriend.org>
2021-04-02 19:49:01 +0200

    Introduce new plan to implement device-register readers and writers
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/chip-remote/device/access.scm
+++ scheme/chip-remote/device/access.scm
@@ -16,20 +16,33 @@
             da-read
             da-write))
 
+(define (default-read c p r v)
+  (transmit c v))
+
+(define (default-write c p r v)
+  (transmit c v))
+
 (define-record-type <device-access>
   (make-device-access* bus transmit read write)
   device-access?
   (bus da-bus)
   (transmit da-transmit)
-  ;; These shoud be interpreter script or scheme procedures too.
+  ;; These shoud be interpreter script or scheme procedures too. If they are
+  ;; scheme procedures, they take four arguments: connection, page-address,
+  ;; register-address and value. They directly call the protocol primitives
+  ;; that talk to a device.
+  ;;
+  ;; The interpreter script variant isn't implemented yet. The plan is to put
+  ;; the connection into the interpreter's environment and then only take the
+  ;; three latter arguments that the scheme functions do.
   (read da-read)
   (write da-write))
 
 (define* (make-device-access #:key
                              (bus (make-device-access-spi))
                              (transmit (make-device-transmit))
-                             (read (lambda (pa ra) ra))
-                             (write (lambda (pa ra value) value)))
+                             (read default-read)
+                             (write default-write))
   (make-device-access* bus transmit read write))
 
 (define (access-bus->proc bus)
