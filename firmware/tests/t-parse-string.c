faaa43a77d649ac69051ed457630dd5a1625f943
Frank Terbeck <ft@bewatermyfriend.org>
2021-04-02 22:10:10 +0200

    Fix firmware protocol test-suite and remove libtap dependency
    Recent versions of ufw have a small, portable TAP emitting testing API. We're
    now using this. The final goal would be to run this test-suite on foreign
    targets, like cortex-m3, via qemu.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- firmware/tests/t-parse-string.c
+++ firmware/tests/t-parse-string.c
@@ -10,13 +10,14 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-#include <tap.h>
-#include <c/compat.h>
-#include <common/compiler.h>
-
 #include <parse-string.h>
+
+#include <test/tap.h>
+#include <c/compat/strings.h>
+#include <common/compiler.h>
 
 #define BUFFER_SIZE 1024u
 char test_buffer[BUFFER_SIZE];
@@ -29,24 +30,32 @@ basic_failure(enum cr_proto_result code)
            code);
 }
 
+static char reply_buffer[BUFFER_SIZE];
+
+static void
+mem_sink(const char *buf)
+{
+    strlcpy(reply_buffer, buf, BUFFER_SIZE);
+}
+
 static void
 test_command_hi(void)
 {
     struct cr_proto_parse result;
     enum cr_proto_result code;
 
+    strlcpy(reply_buffer, "", sizeof(test_buffer));
     strlcpy(test_buffer, "HI", sizeof(test_buffer));
 
-    code = cr_parse_string(test_buffer, &result);
+    code = cr_parse_string(mem_sink, test_buffer, &result);
     bool success = code == CR_PROTO_RESULT_OK;
     ok(success, "HI parses ok");
 
     if (success == false) {
         basic_failure(code);
     } else {
-        cmp_ok(result.cmd->id, "==", CR_PROTO_CMD_HI,
-               "HI result has correct id");
-        cmp_ok(result.argn, "==", 0, "HI takes no arguments");
+        ok(result.cmd->id == CR_PROTO_CMD_HI, "HI result has correct id");
+        ok(result.argn == 0, "HI takes no arguments");
     }
 }
 
