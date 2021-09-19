4e22c715e0cf790b48df82fb248fae406d93f652
Frank Terbeck <ft@bewatermyfriend.org>
2021-09-19 15:46:38 +0200

    lib,test: Add instrumentation connection to test-io datum
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/test/chip-remote.scm
+++ scheme/test/chip-remote.scm
@@ -18,6 +18,8 @@
             make-test-io
             tio-unknown!
             tio-connection
+            tio-iconnection
+            tio-instrumentation
             set-tio-connection!
             tio-fw-port
             set-tio-fw-port!
@@ -25,11 +27,13 @@
             set-tio-pid!
             tio-terminal
             set-tio-terminal!
+            set-tio-instrumentation!
             connect-test-io!
             handle-stdin!
             kill-fw!
             boot-fw!
-            $))
+            $
+            instrument!))
 
 (define* (init-connection #:key (device (getenv "CR_BOARD_DEVICE")))
   (let ((c (make-cr-connection device)))
@@ -67,18 +71,21 @@
     (quit 1)))
 
 (define-record-type <test-io>
-  (make-test-io* fw-port terminal connection pid)
+  (make-test-io* fw-port terminal instrumentation connection iconnection pid)
   test-io?
   (fw-port tio-fw-port set-tio-fw-port!)
   (terminal tio-terminal set-tio-terminal!)
+  (instrumentation tio-instrumentation set-tio-instrumentation!)
   (connection tio-connection set-tio-connection!)
+  (iconnection tio-iconnection set-tio-iconnection!)
   (pid tio-pid set-tio-pid!))
 
-(define* (make-test-io #:optional terminal)
-  (make-test-io* #f terminal #f #f))
+(define* (make-test-io #:optional terminal instrumentation)
+  (make-test-io* #f terminal instrumentation #f #f #f))
 
 (define (connect-test-io! io)
-  (set-tio-connection! io (chip-remote-open! #:uri (tio-terminal io))))
+  (set-tio-connection! io (chip-remote-open! #:uri (tio-terminal io)))
+  (set-tio-iconnection! io (open-file (tio-instrumentation io) "r+l")))
 
 (define (handle-xread-timeout tio rv)
   (format #t "# xread timeout from firmware. Giving up!~%")
@@ -120,6 +127,7 @@
 
 (define *s-exp-boot-tag* "(activated!)")
 (define *cr-terminal* "UART_0")
+(define *cr-instrumentation* "UART_1")
 
 (define (boot-fw! tio)
   (format #t "# Booting native firmware: ~a~%" (native-fw))
@@ -127,17 +135,35 @@
   (let loop ((line (read-line (tio-fw-port tio) 'trim)))
     (unless (string= line *s-exp-boot-tag*)
       (let ((lst (string-split line #\space)))
-        (when (and (not (null? lst))
-                   (string= (car lst) *cr-terminal*))
-          (set-tio-terminal! tio (car (reverse lst)))))
+        (cond ((and (not (null? lst))
+                    (string= (car lst) *cr-terminal*))
+               (set-tio-terminal! tio (car (reverse lst))))
+              ((and (not (null? lst))
+                    (string= (car lst) *cr-instrumentation*))
+               (set-tio-instrumentation! tio (car (reverse lst))))))
       (loop (read-line (tio-fw-port tio) 'trim))))
 
   (if tio
       (begin
         (format #t "# Chip-Remote Terminal at: ~a~%" (tio-terminal tio))
+        (format #t "# Instrumentation Terminal at: ~a~%"
+                (tio-instrumentation tio))
         (format #t "# S-Expression Interface up.~%")
         (connect-test-io! tio))
       (tio-unknown!))
 
   ;; The firmware should indicate its PID first thing in s-exp mode.
   (handle-stdin! tio))
+
+(define (instrument! tio exp)
+  (format (tio-iconnection tio) "~a~%" exp)
+  ;; The instrumentation request needs to return an ‘ok’ on the instrumentation
+  ;; port. This is done after all processing of the request.
+  (match (xread (tio-iconnection tio) #:timeout 2)
+    ('ok #t)
+    (reply (throw 'expected-ok-from-instrumentation reply)))
+  ;; The firmware echos the instrumentation request on its output port again.
+  ;; This is mainly for debuggability, but we can certainly test for it.
+  (match (xread (tio-fw-port tio) #:timeout 2)
+    (('instrumentation rest ...) #t)
+    (_ (throw 'expected-instrumentation-reply))))
