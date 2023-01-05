3d861bb252a3a85a3f30edcfcf7ac068de0a0cda
Frank Terbeck <ft@bewatermyfriend.org>
2023-01-05 05:00:51 +0100

    Add dir-locals file for some emacs convenience
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil
+  (meta:project-author  . "chip-remote workers")
+  (meta:project-licence . "LICENCE")
+  (meta:project-code "scheme"
+                     "firmware/application"
+                     "firmware/libraries")))
