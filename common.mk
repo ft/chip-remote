4b1ac1f6a36633f2b430e1fbc97aa9a752e65f96
Frank Terbeck <ft@bewatermyfriend.org>
2023-04-05 21:07:57 +0200

    Add csv reader from guile-csv
    Admittedly, I should just use this as an external module. But I need this to
    work in environments where these sort of external resources are tough. So for
    now, I'll keep it in here.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- common.mk
+++ common.mk
@@ -78,6 +78,7 @@ MODULES_DEVICES += $(CR)/devices/texas-instruments/cdce72010.scm
 
 MODULES_PROTO = $(CRP)/slip.scm
 MODULES_PROTO += $(CRP)/length-prefix.scm
+MODULES_PROTO += $(CRP)/csv.scm
 
 MODULES_DATASTRUCT = $(CRDS)/loadable-fifo.scm
 MODULES_DATASTRUCT += $(CRDS)/sized-stack.scm
