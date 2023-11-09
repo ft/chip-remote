79192df64473ef3ff615d494e999f32021e51ed5
Frank Terbeck <ft@bewatermyfriend.org>
2023-11-09 18:19:27 +0100

    test: Allow users to premit holes in register tables
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/test/validate/items-no-holes.scm
+++ scheme/test/validate/items-no-holes.scm
@@ -7,11 +7,15 @@
   #:use-module (test tap)
   #:use-module (test validate items-no-holes-and-overlap)
   #:use-module (chip-remote device)
+  #:use-module (chip-remote utilities)
   #:export (items-no-holes/count
             items-no-holes/check))
 
-(define items-no-holes/count items-alignment-count)
+(define (items-no-holes/count dev cfg)
+  (let ((skip? (kwa-ref cfg #:allow-holes? #f)))
+    (if skip? 0 (items-alignment-count dev cfg))))
 (define-alignment-test test-holes pass-if->= " don't form holes")
 (define check-reg (make-alignment-test test-holes))
 (define (items-no-holes/check dev cfg)
-  (for-each check-reg (device-registers dev)))
+  (let ((skip? (kwa-ref cfg #:allow-holes? #f)))
+    (if skip? #t (for-each check-reg (device-registers dev)))))
