ebe57dffcdea5cda80cdc5c9075f541b595fbfa2
Frank Terbeck <ft@bewatermyfriend.org>
2024-01-04 03:21:34 +0100

    dir-locals: Set up guile-tap related indentation rules
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,11 @@
   (meta:project-licence . "LICENCE")
   (meta:project-code "scheme"
                      "firmware/application"
-                     "firmware/libraries")))
+                     "firmware/libraries"))
+ (scheme-mode
+  (eval . (put 'for-each-test            'scheme-indent-function 1))
+  (eval . (put 'with-test-bundle         'scheme-indent-function 1))
+  (eval . (put 'with-fs-test-bundle      'scheme-indent-function 0))
+  (eval . (put 'with-ellipsis            'scheme-indent-function 1))
+  (eval . (put 'set-record-type-printer! 'scheme-indent-function 1))
+  (eval . (put 'define-test              'scheme-indent-function 1))))
