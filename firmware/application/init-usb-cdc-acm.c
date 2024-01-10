0e8336ad993880d163b411f833a4bedceda75934
Frank Terbeck <ft@bewatermyfriend.org>
2024-01-10 19:29:42 +0100

    fw: Move nucleo-f767zi port to new protocol
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- firmware/application/init-usb-cdc-acm.c
+++ firmware/application/init-usb-cdc-acm.c
@@ -5,35 +5,64 @@
 #include <zephyr/usb/usb_device.h>
 
 #include <ufw/compiler.h>
+#include <ufw/endpoints.h>
+#include <ufw/register-protocol.h>
+#include <ufwz/endpoint-uart-poll.h>
 
-#include "init-common.h"
-
-const struct device *uart0;
-
-void
-uart_sink(const char *str)
-{
-    const size_t len = strlen(str);
-    uart_fifo_fill(uart0, str, len);
-}
-
-#if 0
-static void
-cr_handle_usb(const struct device *dev, UNUSED void *userdata)
-{
-    char ch;
-    while (uart_fifo_read(dev, &ch, 1u) > 0u)
-        cr_toplevel(&proto, ch);
-}
-#endif
+#include "chip-remote.h"
+#include "peripherals.h"
+#include "registers.h"
 
 #define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static void
+led0_heartbeat(UNUSED void *p1, UNUSED void *p2, UNUSED void *p3)
+{
+    if (device_is_ready(led0.port) == false) {
+        printk("Could not access heartbeat LED.\n");
+        return;
+    }
+
+    int ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        printk("Could not configure LED pin.\n");
+        return;
+    }
+
+    enum { ON, OFF } state = OFF;
+    const unsigned int offtimes[] = { 50, 900 };
+    const unsigned int ontime = 100;
+    unsigned int n = 0u;
+
+    for (;;) {
+        switch (state) {
+        case OFF:
+            k_msleep(offtimes[n%2u]);
+            gpio_pin_set_dt(&led0, 1);
+            state = ON;
+            break;
+        case ON:
+            k_msleep(ontime);
+            gpio_pin_set_dt(&led0, 0);
+            state = OFF;
+            break;
+        }
+    }
+}
+
+#define LED0_HEARTBEAT_STACKSIZE 128
+#define LED0_HEARTBEAT_PRIORITY   12
+
+K_THREAD_DEFINE(led0_headerbeat_thread,
+                LED0_HEARTBEAT_STACKSIZE,
+                led0_heartbeat, NULL, NULL, NULL,
+                LED0_HEARTBEAT_PRIORITY, 0, 0);
 
 void
 main(void)
 {
-    uart0 = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
+    const struct device *uart0 = DEVICE_DT_GET(DT_NODELABEL(cdc_acm_uart0));
     if (uart0 == NULL) {
         printk("Could not access usb. Giving up.\n");
         return;
@@ -41,37 +70,27 @@ main(void)
 
     if (usb_enable(NULL) != 0) {
         printk("Could not enable usb. Giving up.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (peripheral_check() < 0) {
         return;
     }
 
-#if 0
-    printk("Registering usb callback.\n");
-    uart_irq_callback_set(uart0, cr_handle_usb);
-    printk("Enabling usb rx interrupt.\n");
-    uart_irq_rx_enable(uart0);
-#endif
+    RegP protocol;
+    Source regpsource = UFWZ_UART_POLL_SOURCE(uart0);
+    Sink regpsink = UFWZ_UART_POLL_SINK(uart0);
 
-    if (!device_is_ready(led.port)) {
-        printk("Could not access LED.\n");
+    if (chip_remote_init(&protocol, regpsource, regpsink, &registers) < 0) {
         return;
     }
 
-    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        printk("Could not configure LED pin.\n");
-        return;
-    }
+    printk("ChipRemoteFirmware running on %s\n", CONFIG_BOARD);
 
-    printk("ChipRemote Command Processor online!\n");
-
-    uint32_t cnt = 0ul;
     for (;;) {
-#if 0
-        if ((cnt % 10u) == 0u)
-            printk("Still here.\n");
-#endif
-        cnt += 1u;
-        gpio_pin_toggle_dt(&led);
-        k_msleep(100);
+        const int rc = chip_remote_process(&protocol);
+        if (rc < 0) {
+            k_usleep(1000);
+        }
     }
 }
