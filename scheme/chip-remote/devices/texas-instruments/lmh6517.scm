e397661fad463c134f8e58ad8b7e41c1a4ee4f9a
Frank Terbeck <ft@bewatermyfriend.org>
2024-04-03 16:47:28 +0200

    lmh6517: Fix registers and their semantics
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/chip-remote/devices/texas-instruments/lmh6517.scm
+++ scheme/chip-remote/devices/texas-instruments/lmh6517.scm
@@ -19,23 +19,26 @@
 (define lmh6517-fixed-gain 22)
 (define lmh6517-attenuation-max 31.5)
 
-
 (define-semantics lmh6517-gain
   (default (static 0))
   (range (lambda (s w) `(range ,(- lmh6517-fixed-gain
                                    lmh6517-attenuation-max)
                                ,lmh6517-fixed-gain)))
   (encode (make-evaluation
-           `(lambda (w x) (increment ,lmh6517-fixed-gain
-                                     (multiply x 2)))))
+           `(lambda (w x) (multiply 2 (decrement ,lmh6517-fixed-gain x)))))
   (decode (make-evaluation
-           `(lambda (w x) (decrement ,lmh6517-fixed-gain
-                                     (multiply x 0.5))))))
+           `(lambda (w x) (decrement ,lmh6517-fixed-gain (multiply 0.5 x))))))
 
-(define-register reg:gain
-  (items (list (reserved 0 1)
-               (‣ gain 1 6 (semantics lmh6517-gain))
-               (‣ enable 7 1))))
+(define (make-lmh-reg ch)
+  (register
+   (items (list (reserved 0 1)
+                (item (name (symbol-append 'gain- ch))
+                      (offset 1)
+                      (width 6)
+                      (semantics lmh6517-gain))
+                (item (name (symbol-append 'enable- ch))
+                      (offset 7)
+                      (width 1))))))
 
 (define-device lmh6517
   (manufacturer texas-instruments)
@@ -47,5 +50,5 @@
    (pm→
     (table
      (↔ (#f (rm→
-             (table (↔ (0 reg:gain)
-                       (1 reg:gain))))))))))
+             (table (↔ (0 (make-lmh-reg 'a))
+                       (1 (make-lmh-reg 'b)))))))))))
